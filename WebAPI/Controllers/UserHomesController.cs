3f5a898a6e890bda5d07da84574ce1be7c8257ff
ibrahimcansun <ibolican2000@gmail.com>
2021-12-14 11:32:56 +0300

    IoC Container

---
+++ WebAPI/Controllers/UserHomesController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserHomesController : ControllerBase
+    {
+        IUserHomeService _userHomeService;
+
+        public UserHomesController(IUserHomeService userHomeService)
+        {
+            _userHomeService = userHomeService;
+        }
+
+        [HttpGet]
+        public List<UserHome> Get()
+        {
+            var result = _userHomeService.GetAll();
+
+            return result.Data;
+        }
+    }
+}
